571ad57692c4b3e94fa459f0d8d18f8c78649808
noogen <tom@noogen.net>
2016-01-17 16:47:19 -0600

    update xstore dependencies

--- xstore.js
+++ xstore.js
@@ -121,7 +121,7 @@
 
     /**
      * defer/promise class
-    #
+     *
      */
     mydeferred = (function() {
       var i, k, len, ref, v;
@@ -216,7 +216,7 @@
       };
 
       myproxy.prototype.handleProxyMessage = function(e) {
-        var d, hash, id, key, method, myCacheBust, mystore, self;
+        var d, error, hash, id, key, method, myCacheBust, mystore, self;
         d = e.data;
         if (typeof d === "string") {
           if (/^xstore-/.test(d)) {
@@ -224,7 +224,7 @@
           } else {
             try {
               d = JSON.parse(d);
-            } catch (_error) {
+            } catch (error) {
               return;
             }
           }
@@ -302,7 +302,7 @@
 
     /**
      * xstore class
-    #
+     *
      */
     xstore = (function() {
       function xstore() {}
@@ -392,7 +392,7 @@
       };
 
       xstore.prototype.handleMessageEvent = function(e) {
-        var d, di, id, self;
+        var d, di, error, id, self;
         self = this;
         d = e.data;
         if (typeof d === "string") {
@@ -401,7 +401,7 @@
           } else {
             try {
               d = JSON.parse(d);
-            } catch (_error) {
+            } catch (error) {
               return;
             }
           }
@@ -1038,7 +1038,7 @@ module.exports = function loadIframe(options, fn){
 
 }, {"is":8,"script-onload":9,"next-tick":10}],
 8: [function(require, module, exports) {
-
+/* globals window, HTMLElement */
 /**!
  * is
  * the definitive JavaScript type testing library
@@ -1058,7 +1058,7 @@ var isActualNaN = function (value) {
   return value !== value;
 };
 var NON_HOST_TYPES = {
-  boolean: 1,
+  'boolean': 1,
   number: 1,
   string: 1,
   undefined: 1
@@ -1117,11 +1117,11 @@ is.empty = function (value) {
   var type = toStr.call(value);
   var key;
 
-  if ('[object Array]' === type || '[object Arguments]' === type || '[object String]' === type) {
+  if (type === '[object Array]' || type === '[object Arguments]' || type === '[object String]') {
     return value.length === 0;
   }
 
-  if ('[object Object]' === type) {
+  if (type === '[object Object]') {
     for (key in value) {
       if (owns.call(value, key)) { return false; }
     }
@@ -1140,9 +1140,8 @@ is.empty = function (value) {
  * @return {Boolean} true if `value` is equal to `other`, false otherwise
  */
 
-is.equal = function (value, other) {
-  var strictlyEqual = value === other;
-  if (strictlyEqual) {
+is.equal = function equal(value, other) {
+  if (value === other) {
     return true;
   }
 
@@ -1153,7 +1152,7 @@ is.equal = function (value, other) {
     return false;
   }
 
-  if ('[object Object]' === type) {
+  if (type === '[object Object]') {
     for (key in value) {
       if (!is.equal(value[key], other[key]) || !(key in other)) {
         return false;
@@ -1167,7 +1166,7 @@ is.equal = function (value, other) {
     return true;
   }
 
-  if ('[object Array]' === type) {
+  if (type === '[object Array]') {
     key = value.length;
     if (key !== other.length) {
       return false;
@@ -1180,15 +1179,15 @@ is.equal = function (value, other) {
     return true;
   }
 
-  if ('[object Function]' === type) {
+  if (type === '[object Function]') {
     return value.prototype === other.prototype;
   }
 
-  if ('[object Date]' === type) {
+  if (type === '[object Date]') {
     return value.getTime() === other.getTime();
   }
 
-  return strictlyEqual;
+  return false;
 };
 
 /**
@@ -1259,7 +1258,7 @@ is.undef = is.undefined = function (value) {
  */
 
 is.args = is.arguments = function (value) {
-  var isStandardArguments = '[object Arguments]' === toStr.call(value);
+  var isStandardArguments = toStr.call(value) === '[object Arguments]';
   var isOldArguments = !is.array(value) && is.arraylike(value) && is.object(value) && is.fn(value.callee);
   return isStandardArguments || isOldArguments;
 };
@@ -1277,8 +1276,8 @@ is.args = is.arguments = function (value) {
  * @api public
  */
 
-is.array = function (value) {
-  return '[object Array]' === toStr.call(value);
+is.array = Array.isArray || function (value) {
+  return toStr.call(value) === '[object Array]';
 };
 
 /**
@@ -1315,7 +1314,7 @@ is.array.empty = function (value) {
  */
 
 is.arraylike = function (value) {
-  return !!value && !is.boolean(value)
+  return !!value && !is.bool(value)
     && owns.call(value, 'length')
     && isFinite(value.length)
     && is.number(value.length)
@@ -1327,7 +1326,7 @@ is.arraylike = function (value) {
  */
 
 /**
- * is.boolean
+ * is.bool
  * Test if `value` is a boolean.
  *
  * @param {Mixed} value value to test
@@ -1335,8 +1334,8 @@ is.arraylike = function (value) {
  * @api public
  */
 
-is.boolean = function (value) {
-  return '[object Boolean]' === toStr.call(value);
+is.bool = is['boolean'] = function (value) {
+  return toStr.call(value) === '[object Boolean]';
 };
 
 /**
@@ -1349,7 +1348,7 @@ is.boolean = function (value) {
  */
 
 is['false'] = function (value) {
-  return is.boolean(value) && Boolean(Number(value)) === false;
+  return is.bool(value) && Boolean(Number(value)) === false;
 };
 
 /**
@@ -1362,7 +1361,7 @@ is['false'] = function (value) {
  */
 
 is['true'] = function (value) {
-  return is.boolean(value) && Boolean(Number(value)) === true;
+  return is.bool(value) && Boolean(Number(value)) === true;
 };
 
 /**
@@ -1379,7 +1378,7 @@ is['true'] = function (value) {
  */
 
 is.date = function (value) {
-  return '[object Date]' === toStr.call(value);
+  return toStr.call(value) === '[object Date]';
 };
 
 /**
@@ -1416,7 +1415,7 @@ is.element = function (value) {
  */
 
 is.error = function (value) {
-  return '[object Error]' === toStr.call(value);
+  return toStr.call(value) === '[object Error]';
 };
 
 /**
@@ -1434,7 +1433,7 @@ is.error = function (value) {
 
 is.fn = is['function'] = function (value) {
   var isAlert = typeof window !== 'undefined' && value === window.alert;
-  return isAlert || '[object Function]' === toStr.call(value);
+  return isAlert || toStr.call(value) === '[object Function]';
 };
 
 /**
@@ -1451,7 +1450,7 @@ is.fn = is['function'] = function (value) {
  */
 
 is.number = function (value) {
-  return '[object Number]' === toStr.call(value);
+  return toStr.call(value) === '[object Number]';
 };
 
 /**
@@ -1497,7 +1496,7 @@ is.divisibleBy = function (value, n) {
 };
 
 /**
- * is.int
+ * is.integer
  * Test if `value` is an integer.
  *
  * @param value to test
@@ -1505,7 +1504,7 @@ is.divisibleBy = function (value, n) {
  * @api public
  */
 
-is.int = function (value) {
+is.integer = is['int'] = function (value) {
   return is.number(value) && !isActualNaN(value) && value % 1 === 0;
 };
 
@@ -1704,7 +1703,7 @@ is.within = function (value, start, finish) {
  */
 
 is.object = function (value) {
-  return '[object Object]' === toStr.call(value);
+  return toStr.call(value) === '[object Object]';
 };
 
 /**
@@ -1734,7 +1733,7 @@ is.hash = function (value) {
  */
 
 is.regexp = function (value) {
-  return '[object RegExp]' === toStr.call(value);
+  return toStr.call(value) === '[object RegExp]';
 };
 
 /**
@@ -1751,7 +1750,7 @@ is.regexp = function (value) {
  */
 
 is.string = function (value) {
-  return '[object String]' === toStr.call(value);
+  return toStr.call(value) === '[object String]';
 };
 
 /**
@@ -1926,15 +1925,34 @@ module.exports = (function () {
 
 }, {}],
 4: [function(require, module, exports) {
-;(function(win){
+"use strict"
+// Module export pattern from
+// https://github.com/umdjs/umd/blob/master/returnExports.js
+;(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define([], factory);
+    } else if (typeof exports === 'object') {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        module.exports = factory();
+    } else {
+        // Browser globals (root is window)
+        root.store = factory();
+  }
+}(this, function () {
+	
+	// Store.js
 	var store = {},
+		win = (typeof window != 'undefined' ? window : global),
 		doc = win.document,
 		localStorageName = 'localStorage',
 		scriptTag = 'script',
 		storage
 
 	store.disabled = false
-	store.version = '1.3.17'
+	store.version = '1.3.20'
 	store.set = function(key, value) {}
 	store.get = function(key, defaultVal) {}
 	store.has = function(key) { return store.get(key) !== undefined }
@@ -1998,7 +2016,7 @@ module.exports = (function () {
 				callback(key, store.get(key))
 			}
 		}
-	} else if (doc.documentElement.addBehavior) {
+	} else if (doc && doc.documentElement.addBehavior) {
 		var storageOwner,
 			storageContainer
 		// Since #userData storage applies only to specific paths, we need to
@@ -2043,7 +2061,7 @@ module.exports = (function () {
 		// See https://github.com/marcuswestin/store.js/issues/40
 		// See https://github.com/marcuswestin/store.js/issues/83
 		var forbiddenCharsRegex = new RegExp("[!\"#$%&'()*+,/\\\\:;<=>?@[\\]^`{|}~]", "g")
-		function ieKeyFix(key) {
+		var ieKeyFix = function(key) {
 			return key.replace(/^d/, '___$&').replace(forbiddenCharsRegex, '___')
 		}
 		store.set = withIEStorage(function(storage, key, val) {
@@ -2066,8 +2084,8 @@ module.exports = (function () {
 		store.clear = withIEStorage(function(storage) {
 			var attributes = storage.XMLDocument.documentElement.attributes
 			storage.load(localStorageName)
-			for (var i=0, attr; attr=attributes[i]; i++) {
-				storage.removeAttribute(attr.name)
+			for (var i=attributes.length-1; i>=0; i--) {
+				storage.removeAttribute(attributes[i].name)
 			}
 			storage.save(localStorageName)
 		})
@@ -2095,12 +2113,9 @@ module.exports = (function () {
 		store.disabled = true
 	}
 	store.enabled = !store.disabled
-
-	if (typeof module != 'undefined' && module.exports && this.module !== module) { module.exports = store }
-	else if (typeof define === 'function' && define.amd) { define(store) }
-	else { win.store = store }
-
-})(Function('return this')());
+	
+	return store
+}));
 
 }, {}],
 5: [function(require, module, exports) {
@@ -2236,5 +2251,376 @@ function decode(value) {
   }
 }
 
-}, {"debug":2}]}, {}, {"1":""})
+}, {"debug":11}],
+11: [function(require, module, exports) {
+
+/**
+ * This is the web browser implementation of `debug()`.
+ *
+ * Expose `debug()` as the module.
+ */
+
+exports = module.exports = require('./debug');
+exports.log = log;
+exports.formatArgs = formatArgs;
+exports.save = save;
+exports.load = load;
+exports.useColors = useColors;
+exports.storage = 'undefined' != typeof chrome
+               && 'undefined' != typeof chrome.storage
+                  ? chrome.storage.local
+                  : localstorage();
+
+/**
+ * Colors.
+ */
+
+exports.colors = [
+  'lightseagreen',
+  'forestgreen',
+  'goldenrod',
+  'dodgerblue',
+  'darkorchid',
+  'crimson'
+];
+
+/**
+ * Currently only WebKit-based Web Inspectors, Firefox >= v31,
+ * and the Firebug extension (any Firefox version) are known
+ * to support "%c" CSS customizations.
+ *
+ * TODO: add a `localStorage` variable to explicitly enable/disable colors
+ */
+
+function useColors() {
+  // is webkit? http://stackoverflow.com/a/16459606/376773
+  return ('WebkitAppearance' in document.documentElement.style) ||
+    // is firebug? http://stackoverflow.com/a/398120/376773
+    (window.console && (console.firebug || (console.exception && console.table))) ||
+    // is firefox >= v31?
+    // https://developer.mozilla.org/en-US/docs/Tools/Web_Console#Styling_messages
+    (navigator.userAgent.toLowerCase().match(/firefox\/(\d+)/) && parseInt(RegExp.$1, 10) >= 31);
+}
+
+/**
+ * Map %j to `JSON.stringify()`, since no Web Inspectors do that by default.
+ */
+
+exports.formatters.j = function(v) {
+  return JSON.stringify(v);
+};
+
+
+/**
+ * Colorize log arguments if enabled.
+ *
+ * @api public
+ */
+
+function formatArgs() {
+  var args = arguments;
+  var useColors = this.useColors;
+
+  args[0] = (useColors ? '%c' : '')
+    + this.namespace
+    + (useColors ? ' %c' : ' ')
+    + args[0]
+    + (useColors ? '%c ' : ' ')
+    + '+' + exports.humanize(this.diff);
+
+  if (!useColors) return args;
+
+  var c = 'color: ' + this.color;
+  args = [args[0], c, 'color: inherit'].concat(Array.prototype.slice.call(args, 1));
+
+  // the final "%c" is somewhat tricky, because there could be other
+  // arguments passed either before or after the %c, so we need to
+  // figure out the correct index to insert the CSS into
+  var index = 0;
+  var lastC = 0;
+  args[0].replace(/%[a-z%]/g, function(match) {
+    if ('%%' === match) return;
+    index++;
+    if ('%c' === match) {
+      // we only are interested in the *last* %c
+      // (the user may have provided their own)
+      lastC = index;
+    }
+  });
+
+  args.splice(lastC, 0, c);
+  return args;
+}
+
+/**
+ * Invokes `console.log()` when available.
+ * No-op when `console.log` is not a "function".
+ *
+ * @api public
+ */
+
+function log() {
+  // this hackery is required for IE8/9, where
+  // the `console.log` function doesn't have 'apply'
+  return 'object' === typeof console
+    && console.log
+    && Function.prototype.apply.call(console.log, console, arguments);
+}
+
+/**
+ * Save `namespaces`.
+ *
+ * @param {String} namespaces
+ * @api private
+ */
+
+function save(namespaces) {
+  try {
+    if (null == namespaces) {
+      exports.storage.removeItem('debug');
+    } else {
+      exports.storage.debug = namespaces;
+    }
+  } catch(e) {}
+}
+
+/**
+ * Load `namespaces`.
+ *
+ * @return {String} returns the previously persisted debug modes
+ * @api private
+ */
+
+function load() {
+  var r;
+  try {
+    r = exports.storage.debug;
+  } catch(e) {}
+  return r;
+}
+
+/**
+ * Enable namespaces listed in `localStorage.debug` initially.
+ */
+
+exports.enable(load());
+
+/**
+ * Localstorage attempts to return the localstorage.
+ *
+ * This is necessary because safari throws
+ * when a user disables cookies/localstorage
+ * and you attempt to access it.
+ *
+ * @return {LocalStorage}
+ * @api private
+ */
+
+function localstorage(){
+  try {
+    return window.localStorage;
+  } catch (e) {}
+}
+
+}, {"./debug":12}],
+12: [function(require, module, exports) {
+
+/**
+ * This is the common logic for both the Node.js and web browser
+ * implementations of `debug()`.
+ *
+ * Expose `debug()` as the module.
+ */
+
+exports = module.exports = debug;
+exports.coerce = coerce;
+exports.disable = disable;
+exports.enable = enable;
+exports.enabled = enabled;
+exports.humanize = require('ms');
+
+/**
+ * The currently active debug mode names, and names to skip.
+ */
+
+exports.names = [];
+exports.skips = [];
+
+/**
+ * Map of special "%n" handling functions, for the debug "format" argument.
+ *
+ * Valid key names are a single, lowercased letter, i.e. "n".
+ */
+
+exports.formatters = {};
+
+/**
+ * Previously assigned color.
+ */
+
+var prevColor = 0;
+
+/**
+ * Previous log timestamp.
+ */
+
+var prevTime;
+
+/**
+ * Select a color.
+ *
+ * @return {Number}
+ * @api private
+ */
+
+function selectColor() {
+  return exports.colors[prevColor++ % exports.colors.length];
+}
+
+/**
+ * Create a debugger with the given `namespace`.
+ *
+ * @param {String} namespace
+ * @return {Function}
+ * @api public
+ */
+
+function debug(namespace) {
+
+  // define the `disabled` version
+  function disabled() {
+  }
+  disabled.enabled = false;
+
+  // define the `enabled` version
+  function enabled() {
+
+    var self = enabled;
+
+    // set `diff` timestamp
+    var curr = +new Date();
+    var ms = curr - (prevTime || curr);
+    self.diff = ms;
+    self.prev = prevTime;
+    self.curr = curr;
+    prevTime = curr;
+
+    // add the `color` if not set
+    if (null == self.useColors) self.useColors = exports.useColors();
+    if (null == self.color && self.useColors) self.color = selectColor();
+
+    var args = Array.prototype.slice.call(arguments);
+
+    args[0] = exports.coerce(args[0]);
+
+    if ('string' !== typeof args[0]) {
+      // anything else let's inspect with %o
+      args = ['%o'].concat(args);
+    }
+
+    // apply any `formatters` transformations
+    var index = 0;
+    args[0] = args[0].replace(/%([a-z%])/g, function(match, format) {
+      // if we encounter an escaped % then don't increase the array index
+      if (match === '%%') return match;
+      index++;
+      var formatter = exports.formatters[format];
+      if ('function' === typeof formatter) {
+        var val = args[index];
+        match = formatter.call(self, val);
+
+        // now we need to remove `args[index]` since it's inlined in the `format`
+        args.splice(index, 1);
+        index--;
+      }
+      return match;
+    });
+
+    if ('function' === typeof exports.formatArgs) {
+      args = exports.formatArgs.apply(self, args);
+    }
+    var logFn = enabled.log || exports.log || console.log.bind(console);
+    logFn.apply(self, args);
+  }
+  enabled.enabled = true;
+
+  var fn = exports.enabled(namespace) ? enabled : disabled;
+
+  fn.namespace = namespace;
+
+  return fn;
+}
+
+/**
+ * Enables a debug mode by namespaces. This can include modes
+ * separated by a colon and wildcards.
+ *
+ * @param {String} namespaces
+ * @api public
+ */
+
+function enable(namespaces) {
+  exports.save(namespaces);
+
+  var split = (namespaces || '').split(/[\s,]+/);
+  var len = split.length;
+
+  for (var i = 0; i < len; i++) {
+    if (!split[i]) continue; // ignore empty strings
+    namespaces = split[i].replace(/\*/g, '.*?');
+    if (namespaces[0] === '-') {
+      exports.skips.push(new RegExp('^' + namespaces.substr(1) + '$'));
+    } else {
+      exports.names.push(new RegExp('^' + namespaces + '$'));
+    }
+  }
+}
+
+/**
+ * Disable debug output.
+ *
+ * @api public
+ */
+
+function disable() {
+  exports.enable('');
+}
+
+/**
+ * Returns true if the given mode name is enabled, false otherwise.
+ *
+ * @param {String} name
+ * @return {Boolean}
+ * @api public
+ */
+
+function enabled(name) {
+  var i, len;
+  for (i = 0, len = exports.skips.length; i < len; i++) {
+    if (exports.skips[i].test(name)) {
+      return false;
+    }
+  }
+  for (i = 0, len = exports.names.length; i < len; i++) {
+    if (exports.names[i].test(name)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+/**
+ * Coerce `val`.
+ *
+ * @param {Mixed} val
+ * @return {Mixed}
+ * @api private
+ */
+
+function coerce(val) {
+  if (val instanceof Error) return val.stack || val.message;
+  return val;
+}
+
+}, {"ms":7}]}, {}, {"1":""})
 );
